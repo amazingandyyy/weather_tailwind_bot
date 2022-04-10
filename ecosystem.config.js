bfca3826da2547ca87846bc6fbcbb40b68f25e0f
Andy Chen <amazingandyyy@gmail.com>
2022-04-10 12:24:21 +0100

    feat: add pm2 and deploy to heroku

---
+++ ecosystem.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  apps : [{
+    script: 'index.js',
+    watch: '.'
+  }, {
+    script: './service-worker/',
+    watch: ['./service-worker']
+  }],
+
+  deploy : {
+    production : {
+      ref  : 'origin/main',
+      repo : 'amazingandyyy/weather_tailwind_bot',
+      path : '.',
+      'pre-deploy-local': '',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': ''
+    }
+  }
+};
